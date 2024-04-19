52d0d3493c958226eb3c029024a09430c48f3096
Timothy Chico Supan <timsupann@gmail.com>
2024-04-19 12:10:57 -0700

    Create skeleton code for hmtl and js, initial classes

---
+++ script.js
@@ -0,0 +1,15 @@
+// user should be able to search for a specific location
+// user should be able to toggle displaying data in Fahrenheit or Celcius
+
+// background should change based on the data: color of background changes based on weather
+// bonus: use Giphy API to find appropriate weather related gif
+
+const button = document.querySelector('button');
+const input = document.querySelector('location');
+const temp = document.querySelector('temp'); //where temp will be displayed
+
+button.addEventListener('click', function(){
+    const location = input.value;
+    const url = ''
+
+})
