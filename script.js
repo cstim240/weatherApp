a20014a0eb281dbb6ba47fe81561ecaa388263b6
Timothy Chico Supan <timsupann@gmail.com>
2024-04-19 12:32:50 -0700

    Add code to display current Celcius temperature of inputted location

--- script.js
+++ script.js
@@ -4,12 +4,28 @@
 // background should change based on the data: color of background changes based on weather
 // bonus: use Giphy API to find appropriate weather related gif
 
-const button = document.querySelector('button');
-const input = document.querySelector('location');
-const temp = document.querySelector('temp'); //where temp will be displayed
+const button = document.getElementById('btn');
+let input = document.getElementById('location');
+let temp = document.querySelector('.temp'); //where temp will be displayed
 
-button.addEventListener('click', function(){
-    const location = input.value;
-    const url = ''
+button.addEventListener('click', async function(){
+    let location = input.value;
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=ae6aaba6a219490dbba52518241904&q=${location}&days=1&aqi=no&alerts=no`;
+    const data = await fetchData(url);
 
-})
+    if (data){
+        temp.textContent = `${data.current.temp_c} °C`;
+    }
+});
+
+async function fetchData(url){
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return await response.json();
+    } catch (error){
+        console.error('Error:', error);
+    }
+};
